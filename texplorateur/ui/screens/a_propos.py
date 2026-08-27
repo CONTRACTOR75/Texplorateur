c9e218b4e011718935f52bfc38915d1689fd8877
CONTRACTOR75 <aledimathias@gmail.com>
2026-08-27 12:21:20 +0000

    feat(a_propos): Update author display to use prefix and link to portfolio

--- texplorateur/ui/screens/a_propos.py
+++ texplorateur/ui/screens/a_propos.py
@@ -1,9 +1,14 @@
+import webbrowser
+
 import customtkinter as ctk
 
-from ..theme import font_sous_titre, font_titre
+from ..theme import ACCENT_LIEN, ACCENT_LIEN_SURVOL, font_normal, font_sous_titre, font_titre
 from .base import Screen
 
 VERSION = "2.0"
+
+# TODO(CONTRACTOR75) : remplacer par l'URL du portfolio.
+URL_PORTFOLIO = "https://aledi.netlify.app/"
 
 
 class AProposScreen(Screen):
@@ -27,11 +32,26 @@ class AProposScreen(Screen):
             centre, text=self.t("a_propos.description"), font=font_sous_titre(13), justify="center")
         self.label_description.pack(pady=(0, 16))
 
-        self.label_auteur = ctk.CTkLabel(
-            centre, text=self.t("a_propos.auteur"), font=font_sous_titre(12), text_color="gray")
-        self.label_auteur.pack()
+        ligne_auteur = ctk.CTkFrame(centre, fg_color="transparent")
+        ligne_auteur.pack()
+        self.label_auteur_prefixe = ctk.CTkLabel(
+            ligne_auteur, text=self.t("a_propos.auteur_prefixe"), font=font_sous_titre(12), text_color="gray")
+        self.label_auteur_prefixe.pack(side='left')
+        # "CONTRACTOR75" est un nom propre : pas de traduction.
+        self.lien_auteur = ctk.CTkLabel(
+            ligne_auteur, text=" CONTRACTOR75", font=font_normal(12, "bold"),
+            text_color=ACCENT_LIEN, cursor="hand2",
+        )
+        self.lien_auteur.pack(side='left')
+        self.lien_auteur.bind("<Button-1>", self._ouvrir_portfolio)
+        self.lien_auteur.bind("<Enter>", lambda e: self.lien_auteur.configure(text_color=ACCENT_LIEN_SURVOL))
+        self.lien_auteur.bind("<Leave>", lambda e: self.lien_auteur.configure(text_color=ACCENT_LIEN))
 
     def retraduire(self):
         self.label_version.configure(text=self.t("a_propos.version", version=VERSION))
         self.label_description.configure(text=self.t("a_propos.description"))
-        self.label_auteur.configure(text=self.t("a_propos.auteur"))
+        self.label_auteur_prefixe.configure(text=self.t("a_propos.auteur_prefixe"))
+
+    def _ouvrir_portfolio(self, event=None):
+        if URL_PORTFOLIO:
+            webbrowser.open(URL_PORTFOLIO)
